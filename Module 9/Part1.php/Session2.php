f0953abf911868ac1b9776a419457a96730cb018
Parvez Shaikh <159991267+ParvesSheikh@users.noreply.github.com>
2025-01-12 21:24:47 +0600

    Delete Module 9/Part1.php directory

--- Module 9/Part1.php/Session2.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-session_start() ;
-echo $_SESSION['userEmail'] ;
-echo $_SESSION['userName'] ;
-echo $_SESSION['userScore'] ;
--- Module 9/Part1.php/Session3.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// session_start() ;
-// session_destroy() ;
-
-session_start() ;
-unset($_SESSION['userEmail']) ;
