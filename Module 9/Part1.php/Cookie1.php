cdae7a77fd4143d954ab4094eda4a2eefa89c55b
Parvez Shaikh <159991267+ParvesSheikh@users.noreply.github.com>
2025-01-12 21:26:41 +0600

    Delete Module 9/Part1.php directory

--- Module 9/Part1.php/Cookie1.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Cookies = Frontend temporary data storage system.
-
-setcookie("Token1","1234").PHP_EOL ;
-setcookie("Token2","456").PHP_EOL ;
-setcookie("Token3","789").PHP_EOL ;
--- Module 9/Part1.php/Cookie2.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo $_COOKIE['Token1'] ;
-echo $_COOKIE['Token2'] ;
-echo $_COOKIE['Token3'] ;
